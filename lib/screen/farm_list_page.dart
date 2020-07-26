0298b41dd76af8b70cf4d1e222ce654cdb077ca4
kim-tang2 <>
2020-07-26 20:14:54 +0900

    farm list page 구현

--- lib/screen/farm_list_page.dart
+++ lib/screen/farm_list_page.dart
@@ -10,6 +10,13 @@ class FarmListPage extends StatelessWidget {
     return Scaffold(
         backgroundColor: blueGradient2,
         body: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+            colors: [blueGradient1, blueGradient2],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: [0.3, 0.7],
+          )),
           child: SafeArea(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -30,6 +37,7 @@ class FarmListPage extends StatelessWidget {
                 ),
                 Container(
                     height: 500,
+                    padding: const EdgeInsets.only(left: 32),
                     child: Swiper(
                       itemCount: Provider.of<MineFarmerData>(context)
                           .data
@@ -37,6 +45,10 @@ class FarmListPage extends StatelessWidget {
                           .length,
                       itemWidth: MediaQuery.of(context).size.width - 2 * 64,
                       layout: SwiperLayout.STACK,
+                      pagination: SwiperPagination(
+                        builder: DotSwiperPaginationBuilder(
+                            activeSize: 20, space: 8),
+                      ),
                       itemBuilder: (context, index) {
                         return Stack(
                           children: <Widget>[
@@ -46,26 +58,69 @@ class FarmListPage extends StatelessWidget {
                                   height: 100,
                                 ),
                                 Card(
+                                  elevation: 8,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(32),
+                                  ),
                                   color: Colors.white,
-                                  child: Column(
-                                    children: <Widget>[
-                                      SizedBox(
-                                        height: 100,
-                                      ),
-                                      Text(
-                                        Provider.of<MineFarmerData>(context).data.sensorUUID[index],
-                                        style: TextStyle(
-                                          fontSize: 44,
-                                          color: const Color(0xff47455f),
-                                          fontWeight: FontWeight.w900,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(32.0),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        SizedBox(
+                                          height: 100,
                                         ),
-                                        textAlign: TextAlign.start,
-                                      ),
-                                    ],
+                                        Text(
+                                          //Provider.of<MineFarmerData>(context).data.sensorUUID[index],
+                                          "Farm $index",
+                                          style: TextStyle(
+                                            fontSize: 44,
+                                            color: const Color(0xff47455f),
+                                            fontWeight: FontWeight.w900,
+                                          ),
+                                          textAlign: TextAlign.left,
+                                        ),
+                                        Text(
+                                          "멋진 배추밭",
+                                          style: TextStyle(
+                                            fontSize: 23,
+                                            color: primaryTextColor,
+                                            fontWeight: FontWeight.w500,
+                                          ),
+                                          textAlign: TextAlign.left,
+                                        ),
+                                        SizedBox(
+                                          height: 32,
+                                        ),
+                                        Row(
+                                          children: <Widget>[
+                                            GestureDetector(
+
+                                              child: Text(
+                                                "밭으로 이동",
+                                                style: TextStyle(
+                                                  fontSize: 18,
+                                                  color: secondaryTextColor,
+                                                  fontWeight: FontWeight.w500,
+                                                ),
+                                                textAlign: TextAlign.left,
+                                              ),
+                                            ),
+                                            Icon(
+                                              Icons.arrow_forward,
+                                              color: secondaryTextColor,
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
                                   ),
                                 )
                               ],
                             ),
+                            Image.asset("assets/images/plant.png"),
                           ],
                         );
                       },
