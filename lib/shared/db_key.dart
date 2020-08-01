cff67a9d6b9578d8b80e86c00f47c875ee71e5f6
kim-tang2 <rlaxogns4504@gmail.com>
2020-08-01 15:32:27 +0900

    Complete InfoPage UI

---
+++ lib/shared/db_key.dart
@@ -0,0 +1,19 @@
+/* Farmer DATA Collection */
+const COLLECTION_FARMER = 'farmers';
+const KEY_EMAIL = 'email';
+const KEY_SENSOR_UUID = 'sensor_uuid';
+
+/* Sensor DATA Collection */
+const COLLECTION_NAME = 'sensor_data';
+const KEY_EC = 'ec';
+const KEY_FAN = 'fan';
+const KEY_HUMIDITY = 'humidity';
+const KEY_LED = 'led';
+const KEY_LIGHT = 'light';
+const KEY_LIQUID_FLOW_RATE = 'liquid_flow_rate';
+const KEY_LIQUID_LEVEL = 'liquid_level';
+const KEY_LIQUID_TEMPERATURE = 'liquid_temperature';
+const KEY_PH = 'ph';
+const KEY_TEMPERATURE = 'temperature';
+const KEY_UUID = 'uuid';
+const KEY_VALVE = 'valve';
---
+++ lib/shared/smartfarmer_constants.dart
@@ -0,0 +1,80 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+const Color smartfarmer_primarycolor = Color.fromRGBO(56, 182, 60, 1);//Color(0xFF0C9869);
+const Color smartfarmer_bgcolor = Color.fromRGBO(244, 244, 244, 1);
+const double smartfarmer_padding = 25.0;
+const double smartfarmer_auth_padding = 30.0;
+
+var appBarColor = Color(0xFF17294b);
+
+var bgColor = Color(0xFF0d193a);
+var buttonColor = Color(0xFF1b335d);
+
+var blueGradient1 = Color(0xff52b2f0);
+var blueGradient2 = Color(0xff4968d3);
+
+var login_Gradient1 = Color(0xFF73AEF5);
+var login_Gradient2 = Color(0xFF61A4F1);
+var login_Gradient3 = Color(0xFF478DE0);
+var login_Gradient4 = Color(0xFF398AE5);
+
+var pinkGradient1 = Color(0xfffc6d86);
+var pinkGradient2 = Color(0xfffed0d1);
+
+
+var temp = 'assets/images/temp.png';
+var humidity = 'assets/images/humidity.png';
+var sun = 'assets/images/sun.png';
+var facebook = 'assets/images/facebook.jpg';
+var kakaotalk = 'assets/images/kakao-talk.png';
+
+
+final notoSansBold = TextStyle(
+  color: Colors.white,
+  fontFamily: 'NotoSans-Bold',
+);
+
+final notoSansRegular = TextStyle(
+  color: Colors.white,
+  fontFamily: 'NotoSans-Regular',
+);
+
+final notoSansMedium = TextStyle(
+  color: Colors.white,
+  fontFamily: 'NotoSans-Medium',
+);
+
+final kBoxDecorationStyle = BoxDecoration(
+  color: Color(0xFF6CA8F1),
+  borderRadius: BorderRadius.circular(10.0),
+  boxShadow: [
+    BoxShadow(
+      color: Colors.black12,
+      blurRadius: 6.0,
+      offset: Offset(0, 2),
+    ),
+  ],
+);
+
+Color primaryTextColor = Color(0xFF414C6B);
+Color secondaryTextColor = Color(0xFFE4979E);
+Color titleTextColor = Colors.white;
+Color contentTextColor = Color(0xff868686);
+Color navigationColor = Color(0xFF6751B5);
+Color gradientStartColor = Color(0xFF0050AC);
+Color gradientEndColor = Color(0xFF9354B9);
+
+Color infoButtonColor = Color.fromRGBO(52, 139, 123, 1);
+Color backgroundColor = Color.fromRGBO(239, 239, 239, 1);
+Color infoGradient1 = Color.fromRGBO(73, 83, 207, 1);
+Color infoGradient2 = Color.fromRGBO(134, 13, 255, 1);
+Color textColor = Color.fromRGBO(0, 59, 73, 1);
+Color cardColor_off = Color.fromRGBO(238, 238, 238, 1);
+Color cardFontColor = Color.fromRGBO(51, 51, 51, 1);
+Color deviderColor = Color.fromRGBO(235, 235, 235, 1);
+Color infoBoxTextColor = Color.fromRGBO(0, 59, 73, 1);
+Color infoBoxResultColor = Color.fromRGBO(73, 51, 0, 1);
+Color noneGraphColor = Color.fromRGBO(226, 239, 237, 1);
+Color todayGraphColor = Color.fromRGBO(93, 59, 222, 1);
